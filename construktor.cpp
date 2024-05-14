66756c7f8b59a7168f9aad81037b1c1b70ad734d
ahmadfauzanisnata <fauzanisnata56@gmail.com>
2024-05-14 15:36:44 +0700

    Menambahkan contoh penggunaan konstruktor berbeda dalam fungsi main dan pemanggilan fungsi cetak untuk setiap objek

--- construktor.cpp
+++ construktor.cpp
@@ -35,8 +35,23 @@ class mahasiswa{
         cout << "Nim = " << nim << endl;
          cout << "Nama = " << nama << endl;
           cout << endl;
+    };
+};
+
+    int main()
+    {
+        mahasiswa mhs1;
+         mahasiswa mhs2(20);
+          mahasiswa mhs3("isnata");
+           mahasiswa mhs4(30, "fauzan");
+
+           mhs1.cetak();
+           mhs2.cetak();
+           mhs3.cetak();
+           mhs4.cetak();
+
+           return 0;
     }
 
     
 
-};
