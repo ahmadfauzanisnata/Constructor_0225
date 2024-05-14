e651bdc0e4110b9f668846055ad8d8b9d8d0308c
ahmadfauzanisnata <fauzanisnata56@gmail.com>
2024-05-14 15:14:06 +0700

    Menambahkan implementasi metode statis nim, setID, dan printAll dalam kelas mahasiswa

--- mbrStatikFunc.cpp
+++ mbrStatikFunc.cpp
@@ -22,3 +22,17 @@ class mahasiswa{
         setID();
     }
 };
+int mahasiswa::nim = 225;
+
+void mahasiswa::setID() 
+{
+    id = ++nim;
+}
+
+void mahasiswa::printAll() 
+{
+    cout << "ID =" << id << endl;
+    cout << "Nama =" << nama << endl;
+    cout << endl;
+}
+
