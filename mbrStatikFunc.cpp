7335f45830f3345558c019f18b4abbe61b688283
ahmadfauzanisnata <fauzanisnata56@gmail.com>
2024-05-14 15:17:28 +0700

    Menambahkan logika pembuatan objek mahasiswa, pengaturan nim dengan metode statis, dan pengaksesan nilai nim di fungsi main

--- mbrStatikFunc.cpp
+++ mbrStatikFunc.cpp
@@ -36,3 +36,21 @@ void mahasiswa::printAll()
     cout << endl;
 }
 
+int main() 
+{
+    mahasiswa mhs1("Sri Dadi");
+    mahasiswa mhs2("Budi Jatmiko");
+    mahasiswa::setNim(225);   //mengakakses nim melalui static member function "setNim"
+    mahasiswa mhs3("Andi Janu");
+    mahasiswa mhs4("Joko Wahono");
+
+    mhs1.printAll();
+    mhs2.printAll();
+    mhs3.printAll();
+    mhs4.printAll();
+
+    cout << "akses dari luar object = " << mahasiswa::getNim() << endl; //Mengakses nim
+    system("pause");
+    return 0;
+}
+
