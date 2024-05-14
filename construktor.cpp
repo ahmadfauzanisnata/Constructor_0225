df56003d67f51d2dbb9cef503fa6ff262b76b39a
ahmadfauzanisnata <fauzanisnata56@gmail.com>
2024-05-14 15:29:25 +0700

    Memperbarui definisi kelas mahasiswa dengan menambahkan beberapa konstruktor

---
+++ construktor.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+class mahasiswa{
+    public:
+    int nim;
+    string nama;
+
+    public:
+    mahasiswa()
+    {
+        nim = 0;
+        nama = "";
+    };
+
+    mahasiswa(int iNim)
+    {
+        nim = iNim;
+    };
+
+    mahasiswa(string iNama)
+    {
+        nama = iNama;
+    };
+
+    mahasiswa(int iNim, string iNama)
+    {
+        nim = iNim;
+        nama = iNama;
+    };
+
+    
+
+};
